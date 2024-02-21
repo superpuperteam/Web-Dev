8923dc489e5c57d0aa598b93250772f25fa5b90a
aminkatut <152725848+superpuperteam@users.noreply.github.com>
2024-02-21 17:48:48 +0600

    Add files via upload

---
+++ lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products: Product[] = products;
+  searchInput: string = '';
+
+  get filteredProducts(): Product[] {
+    const searchTerm = this.searchInput.toLowerCase();
+    return this.products.filter(product => product.name.toLowerCase().includes(searchTerm));
+  }
+
+  shareWA(productName: string, productLink: string) {
+    const shareMessage = `Check out this product: ${productName} - ${productLink}`;
+    const whatsappLink = `https://api.whatsapp.com/send?text=${encodeURIComponent(shareMessage)}`;
+    window.location.href = whatsappLink;
+  }
+
+  shareTG(productName: string, productLink: string) {
+    const shareMessage = `Check out this product: ${productName} - ${productLink}`;
+    const whatsappLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
+    window.location.href = whatsappLink;
+  }
+}
---
+++ lab4/src/app/products.ts
@@ -0,0 +1,115 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  link: string;
+  img: string;
+  rating: number;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple 20W USB-C Power Adapter white',
+    price: 2197,
+    description: 'Type: mains charging. Number of connected devices: 1. Fast charging: Yes. Connector: USB Type-C.',
+    link: 'https://kaspi.kz/shop/p/apple-20w-usb-c-power-adapter-belyi-100984093/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h95/h0e/63961934823454.jpg?format=gallery-medium',
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 13 128Gb Midnight black',
+    price: 298600,
+    description: 'Smartphone Apple iPhone 13 128GB Midnight; Model year. 2022; Display diagonal, inch. 6.1; Display resolution. 2532x1170 ; Matrix type. Super Retina XDR.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-midnight-chernyi-102298404/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-medium',
+    rating: 5,
+
+  },
+  {
+    id: 3,
+    name: 'Xiaomi Redmi 12 4G 8GB/256GB black',
+    price: 72000,
+    description: 'Front-camera. 8 Mpx; Amount of RAM. 8 GB ; Built-in memory capacity. 256 GB; Battery capacity. 5000 mAh; Matrix type. FHD.',
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-12-4g-8-gb-256-gb-chernyi-110918152/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h75/hbc/81335343775774.png?format=gallery-medium',
+    rating: 5,
+
+  },
+  {
+    id: 4,
+    name: 'Apple Watch SE 40 mm black',
+    price: 130000,
+    description: 'Apple Watch SE 40mm and 44mm Aluminum cases with a variety of band styles and colors.',
+    link: 'https://kaspi.kz/shop/p/apple-watch-se-40-mm-chernyi-100568123/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/ha3/h60/63868199403550.jpg?format=gallery-medium',
+    rating: 5,
+
+  },
+  {
+    id: 5,
+    name: 'Samsung Galaxy S23 Ultra 12 GB/256 GB black',
+    price: 448374,
+    description: 'Samsung Galaxy S23 Ultra 5G SM-S918B/DS 256GB 12GB RAM, 200 MP Camera, Factory Unlocked, International Model (Phantom Black) ; Display · 6.8 inches, 114.7 cm2',
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-s23-ultra-12-gb-256-gb-chernyi-109174566/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h5e/h53/69635680763934.jpg?format=gallery-medium',
+    rating: 5,
+
+  },
+  {
+    id: 6,
+    name: 'Samsung EP-TA800NBEGRU black',
+    price: 1805,
+    description: 'Сетевое зарядное устройство Samsung EP-TA800 с выходной мощностью 25Вт позволит максимально быстро зарядить совместимый со стандартом Super Fast Charging',
+    link: 'https://kaspi.kz/shop/p/samsung-ep-ta800nbegru-chernyi-100972924/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/hfe/h55/64124143173662.jpg?format=gallery-medium',
+    rating: 3.5,
+
+  },
+  {
+    id: 7,
+    name: 'Xiaomi Redmi Watch 3 Active black',
+    price: 17900,
+    description: 'Xiaomi Redmi Watch 3 Active Smart Watch Fitness Tracker with 100 Sport Modes, Blood Oxygen Heart Rate Sleep Monitor, Bluetooth Phone Call Watch for iPhone',
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-watch-3-active-chernyi-chernyi-111458311/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/he5/h68/81808098557982.jpg?format=gallery-medium',
+    rating: 4.5,
+
+  },
+  {
+    id: 8,
+    name: 'Samsung Galaxy A05s 4GB/128GB purple',
+    price: 66033,
+    description: 'Samsung Galaxy A05s Android smartphone. Announced Sep 2023. Features 6.7″ display, Snapdragon 680 4G chipset, 5000 mAh battery, 128 GB storage, 6 GB RAM.',
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-a05s-4-gb-128-gb-fioletovyi-114693084/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h61/hd2/84523124916254.png?format=gallery-medium',
+    rating: 4,
+  },
+  {
+    id: 9,
+    name: 'Apple iPhone 15 Pro Max 256Gb gray',
+    price: 614343,
+    description: ' Titanium design. A17 Pro chip. 48MP Main camera. Record spatial video for Apple Vision Pro. Action button. USB-C.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-15-pro-max-256gb-seryi-113138420/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/hc1/h65/83559848181790.png?format=gallery-medium',
+    rating: 5,
+  },
+  {
+    id: 10,
+    name: 'Huawei Watch GT 4 46 mm silver-brown',
+    price: 65599,
+    description: 'With a new octagonal design, an innovative strap fastening and a larger AMOLED screen, the HUAWEI WATCH GT 4 is a versatile smartwatch',
+    link: 'https://kaspi.kz/shop/p/huawei-watch-gt-4-46-mm-serebristyi-korichnevyi-113401498/?c=750000000',
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h3a/h59/83882243915806.jpg?format=gallery-medium',
+    rating: 4.5,
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
